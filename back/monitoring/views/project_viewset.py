dbc9af44428b5438ac2a4e9b4590385fb8407411
Elías Gago <egago@icarto.es>
2022-01-03 16:57:54 +0100

    New attribute to identify closed projects and filter in API

--- back/monitoring/views/project_viewset.py
+++ back/monitoring/views/project_viewset.py
@@ -1,13 +1,29 @@
+from django_filters import rest_framework as filters
 from django_filters.rest_framework import DjangoFilterBackend
 from monitoring.models.project import Project
 from monitoring.serializers.project_serializer import ProjectSerializer
 from rest_framework import viewsets
 
 
+class ProjectFilter(filters.FilterSet):
+    status = filters.CharFilter(method="filter_by_status")
+
+    def filter_by_status(self, queryset, name, status):
+        if status == "active":
+            return queryset.filter(closed=False)
+
+        return queryset
+
+    class Meta:
+        model = Project
+        fields = ("status",)
+
+
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
     filter_backends = [DjangoFilterBackend]
+    filterset_class = ProjectFilter
 
     def perform_create(self, serializer):
         serializer.validated_data["creation_user"] = self.request.user
