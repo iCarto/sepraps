447aa57fda6bbf43f2611103aaa40cb633ac0cfd
Elías Gago <egago@icarto.es>
2022-01-13 17:06:29 +0100

    Integrate project front-end model with contacts

--- front/src/components/project/container/ViewProjectContactsSubPage.js
+++ front/src/components/project/container/ViewProjectContactsSubPage.js
@@ -1,14 +1,42 @@
-import {ProjectContactsSection} from "../presentational/contacts";
+import {useEffect, useState} from "react";
+import {useOutletContext} from "react-router-dom";
+import {useSearch} from "hooks";
 
 import Grid from "@mui/material/Grid";
 import Container from "@mui/material/Container";
+import {SearchBox, SectionCard} from "components/common/presentational";
+import {ContactsTable} from "components/contacts/presentational";
 
 const ViewProjectContactsSubPage = () => {
+    let project;
+    [project] = useOutletContext();
+
+    const [filteredContacts, setFilteredContacts] = useState(project.contacts);
+    const {searchText, setSearchText, searchFunction} = useSearch("");
+
+    useEffect(() => {
+        setFilteredContacts([...project.contacts].filter(searchFunction));
+    }, [project.contacts, searchText]);
+
+    const handleSearch = data => {
+        setSearchText(data);
+    };
+
     return (
         <Container maxWidth="lg" sx={{my: 3}}>
             <Grid container spacing={3}>
                 <Grid item xs={12}>
-                    <ProjectContactsSection />
+                    <SectionCard title="Contactos del proyecto">
+                        <Grid container sx={{mb: 2}}>
+                            <Grid item xs={12} md={4}>
+                                <SearchBox
+                                    searchValue={searchText}
+                                    handleSearch={handleSearch}
+                                />
+                            </Grid>
+                        </Grid>
+                        <ContactsTable contacts={filteredContacts} />
+                    </SectionCard>
                 </Grid>
             </Grid>
         </Container>
