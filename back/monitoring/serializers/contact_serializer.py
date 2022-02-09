4b0cf434a61ea9f57d84817e781a4a88c48fa8e2
Elías Gago <egago@icarto.es>
2022-02-09 14:30:57 +0100

    Simplify contacts modification in project and provider endpoints

--- back/monitoring/serializers/contact_serializer.py
+++ back/monitoring/serializers/contact_serializer.py
@@ -3,6 +3,29 @@ from monitoring.models.domain_entry import dominio_get_value
 from rest_framework import serializers
 
 
+class ContactListSerializer(serializers.ListSerializer):
+    def update(self, contacts, validated_data):
+        if validated_data is None:
+            return contacts
+
+        contact_mapping = {contact.id: contact for contact in contacts}
+        data_mapping = {item["id"]: item for item in validated_data}
+
+        ret = []
+        for contact_id, data in data_mapping.items():
+            contact = contact_mapping.get(contact_id, None)
+
+            if contact_id is None:
+                ret.append(self.child.create(data))
+                continue
+
+            if contact is None:
+                contact = Contact.objects.get(pk=contact_id)
+            ret.append(self.child.update(contact, data))
+
+        return ret
+
+
 class ContactSerializer(serializers.ModelSerializer):
 
     id = serializers.IntegerField(allow_null=True)
@@ -20,6 +43,13 @@ class ContactSerializer(serializers.ModelSerializer):
             "email",
             "comments",
         )
+        list_serializer_class = ContactListSerializer
+
+    @classmethod
+    def many_init(cls, *args, **kwargs):
+        # Return ContactListSerializer when many=True
+        kwargs["child"] = cls()
+        return ContactListSerializer(*args, **kwargs)
 
     def get_post_name(self, obj):
         return dominio_get_value(obj.post)
--- back/monitoring/serializers/provider_serializer.py
+++ back/monitoring/serializers/provider_serializer.py
@@ -12,7 +12,7 @@ class ProviderSerializer(serializers.ModelSerializer):
     id = serializers.IntegerField(allow_null=True, required=False)
     locality = LocalitySerializer()
     project = serializers.PrimaryKeyRelatedField(
-        write_only=True, queryset=Project.objects.all()
+        write_only=True, queryset=Project.objects.all(), required=False
     )
     contacts = ContactSerializer(many=True, required=False)
 
@@ -26,48 +26,28 @@ class ProviderSerializer(serializers.ModelSerializer):
         locality_data = validated_data.pop("locality")
         locality = Locality.objects.get(pk=locality_data["code"])
 
+        contacts_data = validated_data.pop("contacts", None)
+
         project = validated_data.pop("project")
 
         provider = Provider.objects.create(**validated_data, locality=locality)
+
+        provider.contacts.set(self.fields["contacts"].update([], contacts_data))
 
         project.provider = provider
         project.save()
 
         return provider
 
-    def update_contacts(self, instance, validated_data):
-        # get the nested objects list
-        contact_data_items = validated_data.pop("contacts", None)
-        contacts_for_project = []
-
-        if contact_data_items:
-            # get all nested objects related with this instance and make a dict(id, object)
-            contact_items_dict = dict((i.id, i) for i in instance.contacts.all())
-
-            for contact_data in contact_data_items:
-                if "id" in contact_data and contact_data["id"] is not None:
-                    # if exists id remove from the dict and update
-                    contact = contact_items_dict.pop(contact_data["id"], None)
-                    if not contact:
-                        contact = Contact.objects.get(pk=contact_data["id"])
-                    # update attributes with validated data
-                    for key in contact_data.keys():
-                        setattr(
-                            contact, key, contact_data.get(key, getattr(contact, key))
-                        )
-
-                    contact.save()
-                    contacts_for_project.append(contact)
-                else:
-                    # else create a new object
-                    contact = Contact.objects.create(**contact_data)
-                    contacts_for_project.append(contact)
-
-        instance.contacts.set(contacts_for_project)
-
     def update(self, instance, validated_data):
 
-        self.update_contacts(instance, validated_data)
+        # calling to ContactListSerializer.update() we can make all modifications
+        # for contacts inside the provider
+        instance.contacts.set(
+            self.fields["contacts"].update(
+                instance.contacts.all(), validated_data.pop("contacts", None)
+            )
+        )
 
         locality_data = validated_data.pop("locality")
         locality = Locality.objects.get(pk=locality_data["code"])
