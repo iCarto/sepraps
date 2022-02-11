a3ed30eae45774ceb925315dc7d579e48fbab96c
Elías Gago <egago@icarto.es>
2022-02-11 19:35:25 +0100

    Update contract model to allow null values

--- back/monitoring/models/construction_contract.py
+++ back/monitoring/models/construction_contract.py
@@ -6,7 +6,7 @@ class ConstructionContract(models.Model):
 
     id = models.AutoField(primary_key=True)
     number = models.CharField("Nombre", max_length=50)
-    comments = models.TextField("Observaciones", max_length=500)
+    comments = models.TextField("Observaciones", max_length=500, null=True)
 
     bid_request_number = models.CharField("Número de llamado", max_length=50)
     bid_request_id = models.CharField("Identificador de llamado", max_length=50)
@@ -17,18 +17,28 @@ class ConstructionContract(models.Model):
     bid_request_deadline = models.IntegerField("Plazo previsto")
 
     awarding_budget = models.DecimalField(
-        "Monto total de adjudicación", max_digits=20, decimal_places=2
+        "Monto total de adjudicación", max_digits=20, decimal_places=2, null=True
     )
     awarding_percentage_drop = models.DecimalField(
-        "Monto total de adjudicación", max_digits=5, decimal_places=2
+        "Monto total de adjudicación", max_digits=5, decimal_places=2, null=True
     )
-    awarding_date = models.DateField("Fecha de adjudicación")
+    awarding_date = models.DateField("Fecha de adjudicación", null=True)
 
-    execution_signature_date = models.DateField("Fecha de firma del contrato")
-    execution_order_start_date = models.DateField("Fecha de la orden de inicio")
-    execution_certificate_start_date = models.DateField("Fecha del acta de inicio")
-    execution_expected_delivery_date = models.DateField("Fecha de recepción provisoria")
-    execution_final_delivery_date = models.DateField("Fecha de recepción definitiva")
+    execution_signature_date = models.DateField(
+        "Fecha de firma del contrato", null=True
+    )
+    execution_order_start_date = models.DateField(
+        "Fecha de la orden de inicio", null=True
+    )
+    execution_certificate_start_date = models.DateField(
+        "Fecha del acta de inicio", null=True
+    )
+    execution_expected_delivery_date = models.DateField(
+        "Fecha de recepción provisoria", null=True
+    )
+    execution_final_delivery_date = models.DateField(
+        "Fecha de recepción definitiva", null=True
+    )
 
     closed = models.BooleanField(blank=False, null=False, default=False)
     creation_user = models.ForeignKey(get_user_model(), on_delete=models.PROTECT)
