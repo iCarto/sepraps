aa12797cca5225418066f256417b526e403a4719
Elías Gago <egago@icarto.es>
2022-02-20 19:49:03 +0100

    Remove contacts step from project creation form

--- front/src/components/project/presentational/form/index.js
+++ front/src/components/project/presentational/form/index.js
@@ -5,4 +5,3 @@ export {
 } from "./ProjectFormGeneralDataSection";
 export {default as ProjectFormLocationSection} from "./ProjectFormLocationSection";
 export {default as ProjectFormProviderSection} from "./ProjectFormProviderSection";
-export {default as ProjectFormContactSection} from "./ProjectFormContactSection";
