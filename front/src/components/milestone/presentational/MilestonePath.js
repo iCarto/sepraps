da2b7d1fc6e25ccb7e5fc89a7b91760148fc24bf
Elías Gago <egago@icarto.es>
2022-03-04 21:04:23 +0100

    First test with timeline layout for milestones

--- front/src/components/milestone/presentational/MilestonePath.js
+++ front/src/components/milestone/presentational/MilestonePath.js
@@ -2,47 +2,46 @@ import {Fragment} from "react";
 
 import MilestonePoint from "./MilestonePoint";
 
-import Grid from "@mui/material/Grid";
+import {Timeline} from "@mui/lab";
 
 const MilestonePath = ({milestones, level, activeMilestone}) => {
     return (
-        <Grid container spacing={3}>
-            {milestones.map(milestone => {
+        <Timeline sx={{ml: level * 5}}>
+            {milestones.map((milestone, index) => {
                 if (milestone.children.length) {
                     return (
                         <Fragment key={milestone.category}>
-                            <Grid item xs={12}>
-                                <MilestonePoint
-                                    milestone={milestone}
-                                    level={level}
-                                    isActiveMilestone={
-                                        activeMilestone.category === milestone.category
-                                    }
-                                />
-                            </Grid>
-                            <Grid item xs={12}>
-                                <MilestonePath
-                                    milestones={milestone.children}
-                                    level={level + 1}
-                                    activeMilestone={activeMilestone}
-                                />
-                            </Grid>
+                            <MilestonePoint
+                                milestone={milestone}
+                                level={level}
+                                isFirst={index === 0}
+                                isLast={index === milestones.length - 1}
+                                isActiveMilestone={
+                                    activeMilestone.category === milestone.category
+                                }
+                            />
+                            <MilestonePath
+                                milestones={milestone.children}
+                                level={level + 1}
+                                activeMilestone={activeMilestone}
+                            />
                         </Fragment>
                     );
                 }
                 return (
-                    <Grid item xs={12} key={milestone.category}>
-                        <MilestonePoint
-                            milestone={milestone}
-                            level={level}
-                            isActiveMilestone={
-                                activeMilestone.category === milestone.category
-                            }
-                        />
-                    </Grid>
+                    <MilestonePoint
+                        key={milestone.category}
+                        milestone={milestone}
+                        level={level}
+                        isFirst={index === 0}
+                        isLast={index === milestones.length - 1}
+                        isActiveMilestone={
+                            activeMilestone.category === milestone.category
+                        }
+                    />
                 );
             })}
-        </Grid>
+        </Timeline>
     );
 };
 
