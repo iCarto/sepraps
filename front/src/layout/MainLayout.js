656d18ebe650f2939571349c49982c8c5c8285b3
Celiamf <celiamartinfern@gmail.com>
2022-03-11 18:53:43 +0100

    Remove burger button to open left drawer

--- front/src/layout/MainLayout.js
+++ front/src/layout/MainLayout.js
@@ -1,21 +1,14 @@
-import {useState} from "react";
 import {Outlet} from "react-router-dom";
 
-import Header from "./Header";
+import {Header} from "layout";
 import ModuleMenu, {DrawerHeader} from "./ModuleMenu";
 import Box from "@mui/material/Box";
 
 const MainLayout = () => {
-    const [drawerOpened, setDrawerOpened] = useState(false);
-
-    const handleDrawerToggle = () => {
-        setDrawerOpened(drowerOpenedPrev => !drowerOpenedPrev);
-    };
-
     return (
         <Box sx={{display: "flex"}}>
-            <Header onShowDrawer={handleDrawerToggle} />
-            <ModuleMenu open={drawerOpened} onToggle={handleDrawerToggle} />
+            <Header />
+            <ModuleMenu />
             <Box component="main" sx={{flexGrow: 1}}>
                 <DrawerHeader />
                 <Outlet />
--- front/src/layout/index.js
+++ front/src/layout/index.js
@@ -1,3 +1,4 @@
+export {default as Header} from "./Header";
 export {default as MainLayout} from "./MainLayout";
 export {default as ModuleLayout} from "./ModuleLayout";
 export {default as PageLayout} from "./PageLayout";
