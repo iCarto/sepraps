29dfd0f0f8fc000d8f1736f24edf71b7113d77b9
Elías Gago <egago@icarto.es>
2022-03-28 14:18:37 +0200

    SENASA default examples with projects from BID-AECID and FOCEM

--- back/monitoring/models/contractor.py
+++ back/monitoring/models/contractor.py
@@ -7,9 +7,11 @@ class Contractor(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField("Nombre", max_length=100)
     contractor_type = models.CharField("Tipo de contratista", max_length=50, null=True)
+    address = models.TextField("Dirección", max_length=500, null=True)
     phone = models.CharField("Teléfono", max_length=20, null=True)
     email = models.CharField("Correo electrónico", max_length=255, null=True)
     contacts = models.ManyToManyField(Contact)
+    comments = models.TextField("Observaciones", max_length=500, null=True)
 
     class Meta:
         db_table = "contractor"
