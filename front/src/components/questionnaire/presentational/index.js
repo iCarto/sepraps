597f2be4c04287080c8979a3dff8681ce37a03d7
Elías Gago <egago@icarto.es>
2022-04-25 17:03:07 +0200

    Add new table for real values data

--- front/src/components/questionnaire/presentational/index.js
+++ front/src/components/questionnaire/presentational/index.js
@@ -5,6 +5,7 @@ export {default as QuestionnaireInstanceList} from "./QuestionnaireInstanceList"
 export {default as QuestionnaireInstanceTree} from "./QuestionnaireInstanceTree";
 export {default as QuestionnaireInstanceDetail} from "./QuestionnaireInstanceDetail";
 export {default as QuestionnaireInstanceSummary} from "./QuestionnaireInstanceSummary";
+export {default as QuestionnaireFieldTable} from "./QuestionnaireFieldTable";
 export {
     default as QuestionnaireExpectedVsRealFieldChart,
 } from "./QuestionnaireExpectedVsRealFieldChart";
