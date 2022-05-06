3e346a1f2131badca0b0d27c81e36b0533631066
Elías Gago <egago@icarto.es>
2022-05-06 16:29:44 +0200

    Relationship between funds and programs as many to many

--- back/monitoring/models/financing_program.py
+++ back/monitoring/models/financing_program.py
@@ -7,11 +7,10 @@ class FinancingProgram(models.Model):
     id = models.AutoField(primary_key=True)
     short_name = models.CharField("Nombre corto", max_length=30)
     name = models.CharField("Nombre", max_length=100)
-    financing_fund = models.ForeignKey(
-        FinancingFund,
-        on_delete=models.PROTECT,
-        verbose_name=FinancingFund._meta.verbose_name,
+    financing_funds = models.ManyToManyField(
+        FinancingFund, verbose_name=FinancingFund._meta.verbose_name
     )
+    closed = models.BooleanField(blank=False, null=False, default=False)
 
     class Meta:
         db_table = "financing_program"
