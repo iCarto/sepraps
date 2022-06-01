4a2e10c549e682db91bcc2c8dd70297dc9c59c4d
Elías Gago <egago@icarto.es>
2022-06-01 10:46:30 +0200

    Show questionnaires in project menu

---
+++ front/src/components/project/presentational/ProjectMenuQuestionnaires.js
@@ -0,0 +1,50 @@
+import {useState} from "react";
+import List from "@mui/material/List";
+import ListItemText from "@mui/material/ListItemText";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemButton from "@mui/material/ListItemButton";
+import ExpandLessIcon from "@mui/icons-material/ExpandLess";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import Collapse from "@mui/material/Collapse";
+import AssignmentOutlinedIcon from "@mui/icons-material/AssignmentOutlined";
+import MenuListItemLink from "components/common/presentational/MenuListItemLink";
+import BallotOutlinedIcon from "@mui/icons-material/BallotOutlined";
+
+const ProjectMenuQuestionnaires = ({project}) => {
+    const [open, setOpen] = useState(false);
+
+    const handleClick = () => {
+        setOpen(!open);
+    };
+
+    return (
+        project && (
+            <>
+                <ListItemButton onClick={handleClick}>
+                    <ListItemIcon>
+                        <AssignmentOutlinedIcon />
+                    </ListItemIcon>
+                    <ListItemText primary="Seguimiento" />
+                    {open ? <ExpandLessIcon /> : <ExpandMoreIcon />}
+                </ListItemButton>
+                <Collapse in={open} timeout="auto" unmountOnExit>
+                    <List component="div" disablePadding>
+                        {project.questionnaires.map(questionnaire => (
+                            <MenuListItemLink
+                                key={questionnaire.code}
+                                to={`/projects/${project.id}/questionnaires/${questionnaire.code}`}
+                            >
+                                <ListItemIcon>
+                                    <BallotOutlinedIcon />
+                                </ListItemIcon>
+                                <ListItemText primary={questionnaire.name} />
+                            </MenuListItemLink>
+                        ))}
+                    </List>
+                </Collapse>
+            </>
+        )
+    );
+};
+
+export default ProjectMenuQuestionnaires;
