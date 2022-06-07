dbff638fa2b4aa9ae94eb8a2e19999271d16528f
Celia Martín <>
2022-06-07 12:20:24 +0200

    Remove milestone children in front

--- front/src/components/milestone/presentational/MilestonePath.js
+++ front/src/components/milestone/presentational/MilestonePath.js
@@ -8,24 +8,6 @@ const MilestonePath = ({milestones, level, activeMilestone}) => {
     return (
         <Timeline sx={{ml: level * 0.5}}>
             {milestones.map((milestone, index) => {
-                if (milestone.children.length) {
-                    return (
-                        <Fragment key={milestone.code}>
-                            <MilestonePoint
-                                milestone={milestone}
-                                level={level}
-                                activeMilestone={activeMilestone}
-                                isFirst={index === 0}
-                                isLast={index === milestones.length - 1}
-                            />
-                            <MilestonePath
-                                milestones={milestone.children}
-                                level={level + 1}
-                                activeMilestone={activeMilestone}
-                            />
-                        </Fragment>
-                    );
-                }
                 return (
                     <MilestonePoint
                         key={milestone.code}
