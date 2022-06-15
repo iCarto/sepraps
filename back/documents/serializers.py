8df125cf98a52e79bfbb8960b0494a0d73aa2c9d
Elías Gago <egago@icarto.es>
2022-06-15 17:37:28 +0200

    Improve document services preview and download

--- back/documents/serializers.py
+++ back/documents/serializers.py
@@ -4,9 +4,16 @@ from documents.models import MediaNode
 from rest_framework import serializers
 
 
+class HashHyperlinkedIdentityField(serializers.HyperlinkedIdentityField):
+    def get_url(self, obj, view_name, request, format):
+        """Adds a hash in the query string useful for caching resources"""
+        url = super().get_url(obj, view_name, request, format)
+        return url + "?date={}".format(obj.created_at.strftime("%Y%m%d%H%M%S%f"))
+
+
 class MediaNodeSerializer(serializers.ModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        lookup_field="media_path", view_name="media-view"
+    url = HashHyperlinkedIdentityField(
+        lookup_field="media_path", view_name="preview-view"
     )
     creation_user = serializers.CharField(
         source="creation_user.username", required=False
@@ -53,3 +60,13 @@ class MediaLeafNodeSerializer(MediaNodeSerializer):
             "creation_user",
             "created_at",
         )
+
+
+class MediaUrlSerializer(serializers.ModelSerializer):
+    url = HashHyperlinkedIdentityField(
+        lookup_field="media_path", view_name="preview-view"
+    )
+
+    class Meta(object):
+        model = MediaNode
+        fields = ("url",)
