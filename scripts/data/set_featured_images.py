3415370cc97ffefcffd9cf43572d7cbcd2946ad8
Elías Gago <egago@icarto.es>
2022-06-16 18:54:21 +0200

    Documents and featured images for default projects

---
+++ scripts/data/set_featured_images.py
@@ -0,0 +1,18 @@
+from documents.models import MediaNode
+from monitoring.models.project import Project
+
+
+projects_images = [
+    {"project_id": 18, "document_id": 140},
+    {"project_id": 19, "document_id": 145},
+    {"project_id": 20, "document_id": 152},
+    {"project_id": 21, "document_id": 161},
+    {"project_id": 22, "document_id": 164},
+    {"project_id": 23, "document_id": 167},
+]
+
+for project_image in projects_images:
+    project = Project.objects.get(pk=project_image["project_id"])
+    document = MediaNode.objects.get(pk=project_image["document_id"])
+    project.featured_image = document
+    project.save()
