46b005d5301ea47fd8eda3be172d9e4527126c9e
Elías Gago <egago@icarto.es>
2022-06-16 18:54:21 +0200

    Documents and featured images for default projects

--- scripts/reset_and_create_db.sh
+++ scripts/reset_and_create_db.sh
@@ -65,6 +65,8 @@ if [[ -z "${CREATE_EMPTY}" ]]; then
     # To generate fixtures auth
     # python manage.py dumpdata auth.Group users.User --natural-foreign --natural-primary --indent 4 > fixtures_auth.json
     # And remove admin user inside file
+    rm -rf "${this_dir}/../${BACKEND_FOLDER_NAME}/media/"*
+    cp -r "${this_dir}/data/folder_data/"* "${this_dir}/../${BACKEND_FOLDER_NAME}/media/"
     python manage.py loaddata "${this_dir}/data/fixtures_auth.json"
     python manage.py loaddata "${this_dir}/data/fixtures_location.json"
     python manage.py loaddata "${this_dir}/data/fixtures_data.json"
@@ -75,8 +77,9 @@ if [[ -z "${CREATE_EMPTY}" ]]; then
     python manage.py loaddata "${this_dir}/data/fixtures_contrato_20_2019.json"
     python manage.py loaddata "${this_dir}/data/fixtures_contrato_24_2019.json"
     python manage.py loaddata "${this_dir}/data/fixtures_contrato_15_2019.json"
+    python manage.py loaddata "${this_dir}/data/fixtures_documents.json"
     python manage.py shell < "${this_dir}/data/update_milestones.py"
-    cp -r "${this_dir}/data/folder_data/"* "${this_dir}/../${BACKEND_FOLDER_NAME}/media/"
+    python manage.py shell < "${this_dir}/data/set_featured_images.py"
 fi
 
 # In install.sh static folders are created
