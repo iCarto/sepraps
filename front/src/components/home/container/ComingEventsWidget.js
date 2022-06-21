a70d6f9c86df89ee688cc9c179ed449f4b044ff5
Celia Martín <celiamartinfern@gmail.com>
2022-06-21 12:11:02 +0200

    Add pagination to events and notifications

--- front/src/components/home/container/ComingEventsWidget.js
+++ front/src/components/home/container/ComingEventsWidget.js
@@ -1,4 +1,4 @@
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 import {useNavigate} from "react-router-dom";
 import {DateUtil} from "utilities";
 
@@ -6,12 +6,28 @@ import {SectionCard} from "components/common/presentational";
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
-import Divider from "@mui/material/Divider";
 import ListItemButton from "@mui/material/ListItemButton";
+import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
+import Divider from "@mui/material/Divider";
 import styled from "@mui/material/styles/styled";
 
 const ComingEventsWidget = ({events}) => {
+    const [currentPage, setCurrentPage] = useState(1);
     const navigate = useNavigate();
+
+    const recordsPerPage = 4;
+
+    const indexOfLastRecord = currentPage * recordsPerPage;
+    const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+
+    const currentRecords = events.slice(indexOfFirstRecord, indexOfLastRecord);
+
+    const numberOfPages = Math.ceil(events.length / recordsPerPage);
+
+    const handleChange = (event, value) => {
+        setCurrentPage(value);
+    };
 
     const DateCardContainer = styled("div")(({theme}) => ({
         display: "flex",
@@ -28,6 +44,7 @@ const ComingEventsWidget = ({events}) => {
     }));
 
     const btnStyle = {
+        py: {xs: 0, xl: "inherit"},
         "&:hover": {
             backgroundColor: "primary.hover",
         },
@@ -35,49 +52,65 @@ const ComingEventsWidget = ({events}) => {
 
     return (
         <SectionCard headingLabel={null} title="Próximos eventos">
-            <List sx={{width: "100%", pr: 2, pt: 0, mt: -2}}>
-                {events?.map(event => {
-                    return (
-                        <Fragment key={event.id}>
-                            <ListItem sx={{pl: "8px"}}>
-                                <ListItemButton
-                                    sx={btnStyle}
-                                    onClick={() => {
-                                        navigate(`${event.url}`);
-                                    }}
-                                >
-                                    <DateCardContainer>
-                                        <span>{DateUtil.getDayInDate(event.date)}</span>
-                                        <span
-                                            style={{
-                                                textTransform: "uppercase",
-                                                fontWeight: "600",
+            {events.length !== 0 ? (
+                <List sx={{width: "100%", pr: 2, pt: 0, mt: -2}}>
+                    {currentRecords?.map(event => {
+                        return (
+                            <Fragment key={event.id}>
+                                <ListItem sx={{pl: "8px"}}>
+                                    <ListItemButton
+                                        sx={btnStyle}
+                                        onClick={() => {
+                                            navigate(`${event.url}`);
+                                        }}
+                                    >
+                                        <DateCardContainer>
+                                            <span>
+                                                {DateUtil.getDayInDate(event.date)}
+                                            </span>
+                                            <span
+                                                style={{
+                                                    textTransform: "uppercase",
+                                                    fontWeight: "600",
+                                                }}
+                                            >
+                                                {DateUtil.getMonthInDate(event.date)}
+                                            </span>
+                                        </DateCardContainer>
+                                        <ListItemText
+                                            primaryTypographyProps={{
+                                                fontWeight: "medium",
                                             }}
-                                        >
-                                            {DateUtil.getMonthInDate(event.date)}
-                                        </span>
-                                    </DateCardContainer>
-                                    <ListItemText
-                                        primaryTypographyProps={{fontWeight: "medium"}}
-                                        primary={event.title}
-                                        secondary={
-                                            event.message +
-                                            ` dentro de ${DateUtil.getDaysToDate(
-                                                event.date
-                                            )} días`
-                                        }
-                                    />
-                                </ListItemButton>
-                            </ListItem>
-                            <Divider
-                                variant="inset"
-                                component="li"
-                                sx={{ml: "100px"}}
-                            />
-                        </Fragment>
-                    );
-                })}
-            </List>
+                                            primary={event.title}
+                                            secondary={
+                                                event.message +
+                                                ` dentro de ${DateUtil.getDaysToDate(
+                                                    event.date
+                                                )} días`
+                                            }
+                                        />
+                                    </ListItemButton>
+                                </ListItem>
+                                <Divider
+                                    variant="inset"
+                                    component="li"
+                                    sx={{ml: "100px"}}
+                                />
+                            </Fragment>
+                        );
+                    })}
+                    {numberOfPages > 1 && (
+                        <Pagination
+                            count={numberOfPages}
+                            page={currentPage}
+                            onChange={handleChange}
+                            sx={{mt: 3, ml: 2}}
+                        />
+                    )}
+                </List>
+            ) : (
+                <Typography>No hay ninguna fecha próxima que destacar</Typography>
+            )}
         </SectionCard>
     );
 };
--- front/src/components/home/container/NotificationsWidget.js
+++ front/src/components/home/container/NotificationsWidget.js
@@ -1,4 +1,4 @@
-import {Fragment} from "react";
+import {Fragment, useState} from "react";
 import {useNavigate} from "react-router-dom";
 
 import {SectionCard} from "components/common/presentational";
@@ -6,14 +6,31 @@ import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import ListItemText from "@mui/material/ListItemText";
 import ListItemButton from "@mui/material/ListItemButton";
+import Typography from "@mui/material/Typography";
+import Pagination from "@mui/material/Pagination";
 import Divider from "@mui/material/Divider";
 import ListItemIcon from "@mui/material/ListItemIcon";
 import CircleIcon from "@mui/icons-material/Circle";
 
 const NotificationsWidget = ({notifications}) => {
+    const [currentPage, setCurrentPage] = useState(1);
     const navigate = useNavigate();
 
+    const recordsPerPage = 4;
+
+    const indexOfLastRecord = currentPage * recordsPerPage;
+    const indexOfFirstRecord = indexOfLastRecord - recordsPerPage;
+
+    const currentRecords = notifications.slice(indexOfFirstRecord, indexOfLastRecord);
+
+    const numberOfPages = Math.ceil(notifications.length / recordsPerPage);
+
+    const handleChange = (event, value) => {
+        setCurrentPage(value);
+    };
+
     const btnStyle = {
+        py: {xs: 0, xl: "inherit"},
         "&:hover": {
             backgroundColor: "primary.hover",
         },
@@ -21,35 +38,55 @@ const NotificationsWidget = ({notifications}) => {
 
     return (
         <SectionCard headingLabel={null} title="Notificaciones">
-            <List sx={{width: "100%", pr: 2, pt: 0, mt: -2}}>
-                {notifications.map(notification => {
-                    return (
-                        <Fragment key={notification.id}>
-                            <ListItem alignItems="flex-start" sx={{pl: "8px"}}>
-                                <ListItemButton
-                                    sx={btnStyle}
-                                    onClick={() => {
-                                        navigate(`${notification.url}`);
-                                    }}
-                                >
-                                    <ListItemIcon sx={{minWidth: "40px", mt: "10px"}}>
-                                        <CircleIcon
-                                            fontSize="small"
-                                            color={notification.severity}
+            {notifications.length !== 0 ? (
+                <List sx={{width: "100%", pr: 2, pt: 0, mt: -2}}>
+                    {currentRecords?.map(notification => {
+                        return (
+                            <Fragment key={notification.id}>
+                                <ListItem alignItems="flex-start" sx={{pl: "8px"}}>
+                                    <ListItemButton
+                                        sx={btnStyle}
+                                        onClick={() => {
+                                            navigate(`${notification.url}`);
+                                        }}
+                                    >
+                                        <ListItemIcon
+                                            sx={{minWidth: "40px", mt: "10px"}}
+                                        >
+                                            <CircleIcon
+                                                fontSize="small"
+                                                color={notification.severity}
+                                            />
+                                        </ListItemIcon>
+                                        <ListItemText
+                                            primaryTypographyProps={{
+                                                fontWeight: "medium",
+                                            }}
+                                            primary={notification.title}
+                                            secondary={notification.message}
                                         />
-                                    </ListItemIcon>
-                                    <ListItemText
-                                        primaryTypographyProps={{fontWeight: "medium"}}
-                                        primary={notification.title}
-                                        secondary={notification.message}
-                                    />
-                                </ListItemButton>
-                            </ListItem>
-                            <Divider variant="inset" component="li" sx={{ml: "64px"}} />
-                        </Fragment>
-                    );
-                })}
-            </List>
+                                    </ListItemButton>
+                                </ListItem>
+                                <Divider
+                                    variant="inset"
+                                    component="li"
+                                    sx={{ml: "64px"}}
+                                />
+                            </Fragment>
+                        );
+                    })}
+                    {numberOfPages > 1 && (
+                        <Pagination
+                            count={numberOfPages}
+                            page={currentPage}
+                            onChange={handleChange}
+                            sx={{mt: 3, ml: 2}}
+                        />
+                    )}
+                </List>
+            ) : (
+                <Typography>No hay notificaciones para mostrar</Typography>
+            )}
         </SectionCard>
     );
 };
