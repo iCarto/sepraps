7801ac5d32aeb1000bce151b9deb69883379234e
Elías Gago <egago@icarto.es>
2022-07-04 17:56:40 +0200

    Fix problem with stats permissions

--- back/questionnaires/renderers.py
+++ back/questionnaires/renderers.py
@@ -12,35 +12,45 @@ class DataFrameCSVFileRenderer(renderers.BaseRenderer):
     charset = None
     render_style = "binary"
 
-    def render(self, df, media_type=None, renderer_context=None):
-        float_column_names = df.select_dtypes(include=["float64"]).columns.tolist()
-        for column_name in float_column_names:
-            df[column_name] = df[column_name].apply(
-                lambda x: locale.format_string("%.2f", x) if not pd.isna(x) else None
-            )
+    def render(self, response, media_type=None, renderer_context=None):
+        if isinstance(response, pd.DataFrame):
+            df = response
+            float_column_names = df.select_dtypes(include=["float64"]).columns.tolist()
+            for column_name in float_column_names:
+                df[column_name] = df[column_name].apply(
+                    lambda x: locale.format_string("%.2f", x)
+                    if not pd.isna(x)
+                    else None
+                )
 
-        if renderer_context is not None:
-            path = renderer_context["request"].get_full_path()
-            filename = "{}_{}".format(
-                datetime.now().strftime("%Y%m%d%H%M%S"), "_".join(path.split("/")[-2:])
-            )
-            renderer_context["response"][
-                "Content-Disposition"
-            ] = "attachment; filename={}.csv".format(filename)
+            if renderer_context is not None:
+                path = renderer_context["request"].get_full_path()
+                filename = "{}_{}".format(
+                    datetime.now().strftime("%Y%m%d%H%M%S"),
+                    "_".join(path.split("/")[-2:]),
+                )
+                renderer_context["response"][
+                    "Content-Disposition"
+                ] = "attachment; filename={}.csv".format(filename)
 
-        buffer = BytesIO()
-        df.to_csv(buffer, index=False)
-        return buffer.getvalue()
+            buffer = BytesIO()
+            df.to_csv(buffer, index=False)
+            return buffer.getvalue()
+        return response
 
 
 class DataFrameJSONRenderer(renderers.BaseRenderer):
     media_type = "application/json"
     format = "json"
 
-    def render(self, df, media_type=None, renderer_context=None):
-        float_column_names = df.select_dtypes(include=["float64"]).columns.tolist()
-        for column_name in float_column_names:
-            df[column_name] = df[column_name].apply(
-                lambda x: "{0:.2f}".format(x) if not pd.isna(x) else None
-            )
-        return pd.io.json.dumps(df.to_dict(orient="list"))
+    def render(self, response, media_type=None, renderer_context=None):
+        print(response)
+        if isinstance(response, pd.DataFrame):
+            df = response
+            float_column_names = df.select_dtypes(include=["float64"]).columns.tolist()
+            for column_name in float_column_names:
+                df[column_name] = df[column_name].apply(
+                    lambda x: "{0:.2f}".format(x) if not pd.isna(x) else None
+                )
+            return pd.io.json.dumps(df.to_dict(orient="list"))
+        return pd.io.json.dumps(response)
