2e0dd561cbf3e1a7f2f6bd2184c292b95bfd91d6
Francisco Puga <fpuga@icarto.es>
2022-07-23 10:59:24 +0200

    Refactor to move custom project constants from 'auth' to 'config'

---
+++ front/src/config/auth/index.js
@@ -0,0 +1,9 @@
+export const USER = "sepraps_user";
+export const ACCESS_TOKEN_KEY = "accessToken";
+export const REFRESH_TOKEN_KEY = "refreshToken";
+export const ROLES = {
+    EDIT: "edicion",
+    MANAGEMENT: "gestion",
+    VIEW: "visualizacion",
+    SUPERVISION: "supervision",
+};
--- front/src/config/index.js
+++ front/src/config/index.js
@@ -3,3 +3,4 @@ export {default as fakeDataService} from "./file/JsonFileService";
 export {default as dataService} from "./http/FetchService";
 export {default as uploadService} from "./http/XMLHttpRequestService";
 export {default as Storage} from "./storage/Storage";
+export {USER, ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY, ROLES} from "./auth";
