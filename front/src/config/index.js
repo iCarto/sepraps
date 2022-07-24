12f9689aaf91912c78bdf3b67b229e89bd411f4b
Francisco Puga <fpuga@icarto.es>
2022-07-24 10:21:47 +0200

    Refactor NumberUtils, moving locales to 'config' package

--- front/src/config/index.js
+++ front/src/config/index.js
@@ -5,3 +5,8 @@ export {default as uploadService} from "./http/XMLHttpRequestService";
 export {default as Storage} from "./storage/Storage";
 export {USER, ACCESS_TOKEN_KEY, REFRESH_TOKEN_KEY, ROLES} from "./auth";
 export {DATE_FORMATS, USED_LOCALE} from "./i18n";
+export {
+    localCurrencyFormatter,
+    localIntegerFormatter,
+    localDecimalFormatter,
+} from "./i18n";
