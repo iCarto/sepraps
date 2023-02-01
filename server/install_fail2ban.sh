21a1663d1a20e248780e90f64a6ce27c2bb1d6c4
Pablo Mariño Boga <pmarino@icarto.es>
2023-02-01 23:17:51 +0100

    Added Wordpress and PostgreSQL to Fail2ban config

--- server/install_fail2ban.sh
+++ server/install_fail2ban.sh
@@ -6,7 +6,8 @@
 set -euo pipefail
 
 this_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" > /dev/null && pwd)"
-source ${this_dir}/fail2ban-settings/fail2ban.conf
+# shellcheck disable=SC1091
+source "${this_dir}/fail2ban-settings/fail2ban.conf"
 
 reset_configuration() {
     if [[ -f "${FILE_DEF}.conf" ]]; then
@@ -57,7 +58,7 @@ sshd_configuration() {
     # Generate SSH configuration
     local config="[sshd]
         enabled=true
-        logpath=${LOG_SSHD_ERROR}\n"
+        logpath=${LOG_SSHD}\n"
     echo -e "${config}" | sed 's/^[\t ]*//' > "${FILE_SSH}"
 }
 
@@ -128,10 +129,11 @@ apache_configuration() {
 }
 
 redmine_configuration() {
+    # Copy redmine regex filter to filter.d folder
     # https://www.redmine.org/projects/redmine/wiki/HowTo_Configure_Fail2ban_For_Redmine
-    cp ${this_dir}/${FILTER_REDMINE_SOURCE} ${F2B_FILTER_DIR}
+    cp "${this_dir}/${FILTER_REDMINE_SOURCE}" "${F2B_FILTER_DIR}"
 
-    # Generate SSH configuration
+    # Generate redmine configuration
     local config="[redmine]
         enabled  = true
         filter   = redmine
@@ -139,6 +141,46 @@ redmine_configuration() {
         logpath=${LOG_REDMINE_PRODUCTION}\n"
     echo -e "${config}" | sed 's/^[\t ]*//' > "${FILE_REDMINE}"
 }
+
+wordpress_configuration(){
+    # Copy wordpress regex filter to filter.d folder
+    # https://help.clouding.io/hc/es/articles/360019516239-C%C3%B3mo-a%C3%B1adir-una-jail-en-Fail2ban-para-WordPress
+    cp "${this_dir}/${FILTER_WORDPRESS_SOURCE}" "${F2B_FILTER_DIR}"
+    
+    # Generate wordpress configuration
+    local config="[wordpress]
+    enabled = true
+    filter = wordpress
+    port = http,https
+    logpath = ${LOG_APACHE_ERROR}\n"
+    echo -e "${config}" | sed 's/^[\t ]*//' > "${FILE_WORDPRESS}"
+}
+
+postgresql_configuration(){
+    # Copy redmine regex filter to filter.d folder
+    cp "${this_dir}/${FILTER_POSTGRESQL_SOURCE}" "${F2B_FILTER_DIR}"
+
+    # https://talk.plesk.com/threads/howto-secure-a-standard-postgres-port-with-fail2ban.355984/
+    # you'll need to modify /etc/postgresql/<VERSION>/main/postgresql.conf
+    # to add host info to the logfile
+    if [[ -f "${LOG_POSTGRESQL}" ]]; then
+        sed "s/log_line_prefix = '\%m \[\%p\] \%q\%u\@\%d '/log_line_prefix = '\%h \%m \[\%p\] \%q\%u\@\%d '/" ${LOG_POSTGRESQL}    
+    else
+        echo "Error: ${LOG_POSTGRESQL} doesn't exist. Make sure to edit LOG_POSTGRESQL value in \
+              fail2ban-settings/fail2ban.conf to match your installation"
+        exit 1
+    fi
+
+    # Generate postgresql configuration
+    local config="[postgresql]
+    enabled = true
+    filter = postgresql
+    port = 5432
+    logpath = ${LOG_POSTGRESQL}\n"
+    echo -e "${config}" | sed 's/^[\t ]*//' > "${FILE_POSTGRESQL}"
+}
+
+
 
 # Install fail2ban
 apt -y install fail2ban
@@ -165,6 +207,14 @@ fi
 
 if [[ ${REDMINE_ENABLE} ]]; then
     redmine_configuration
+fi
+
+if [[ ${WORDPRESS_ENABLE} ]]; then
+    wordpress_configuration
+fi
+
+if [[ ${POSTGRESQL_ENABLE} ]]; then
+    postgresql_configuration
 fi
 
 # Restart service
