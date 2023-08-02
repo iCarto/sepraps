5ff931c30fecf71bd723ac21b3159cdff1f14a74
Elías Gago <elias.gago@gmail.com>
2023-08-02 10:56:56 +0200

    New fixture for Yaguaron field report

--- scripts/data/set_activity_images.py
+++ scripts/data/set_activity_images.py
@@ -3,19 +3,41 @@ from documents.models import MediaNode
 
 
 activity = FieldReportProjectActivity.objects.get(pk=2)
-document = MediaNode.objects.get(pk=181)
+document = MediaNode.objects.get(pk=179)
 activity.image1 = document
-document = MediaNode.objects.get(pk=182)
+document = MediaNode.objects.get(pk=180)
 activity.image2 = document
-document = MediaNode.objects.get(pk=183)
+document = MediaNode.objects.get(pk=181)
 activity.image3 = document
-document = MediaNode.objects.get(pk=184)
+document = MediaNode.objects.get(pk=182)
 activity.image4 = document
 activity.save()
 
 activity = FieldReportProjectActivity.objects.get(pk=3)
-document = MediaNode.objects.get(pk=185)
+document = MediaNode.objects.get(pk=183)
 activity.image1 = document
-document = MediaNode.objects.get(pk=186)
+document = MediaNode.objects.get(pk=184)
 activity.image2 = document
+activity.save()
+
+activity = FieldReportProjectActivity.objects.get(pk=5)
+document = MediaNode.objects.get(pk=189)
+activity.image1 = document
+document = MediaNode.objects.get(pk=190)
+activity.image2 = document
+document = MediaNode.objects.get(pk=191)
+activity.image3 = document
+document = MediaNode.objects.get(pk=192)
+activity.image4 = document
+activity.save()
+
+activity = FieldReportProjectActivity.objects.get(pk=6)
+document = MediaNode.objects.get(pk=193)
+activity.image1 = document
+document = MediaNode.objects.get(pk=194)
+activity.image2 = document
+document = MediaNode.objects.get(pk=195)
+activity.image3 = document
+document = MediaNode.objects.get(pk=196)
+activity.image4 = document
 activity.save()
