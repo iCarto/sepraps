3735e98f1ce78bdca58963cc83ab5490c31cf4dc
Elías Gago <elias.gago@gmail.com>
2023-08-07 13:07:28 +0200

    Fix problem with image orientation from iPhone

--- back/documents/storage.py
+++ back/documents/storage.py
@@ -1,12 +1,49 @@
 from django.core.files.storage import default_storage
+from PIL import ExifTags, Image
 
 
-def open(file, mode="rb"):
+def rotate_image(orientation, image):
+    if orientation == 3:
+        return image.rotate(180, expand=True)
+    elif orientation == 6:
+        return image.rotate(270, expand=True)
+    elif orientation == 8:
+        return image.rotate(90, expand=True)
+
+
+def fix_jpeg_orientation(filepath):
+    image = Image.open(filepath)
+
+    for orientation in ExifTags.TAGS.keys():
+        if ExifTags.TAGS[orientation] == "Orientation":
+            break
+
+    exif = image._getexif()  # noqa: WPS437
+    exif_orientation = (
+        exif[orientation] if exif and orientation in exif else None  # noqa: WPS441
+    )
+    if exif_orientation:
+        image = rotate_image(exif_orientation, image)
+
+        image.save(filepath)
+        image.close()
+
+
+def open(file, mode="rb"):  # noqa: WPS125
     return default_storage.open(file, mode)
 
 
 def save(path, file):
-    return default_storage.save(path, file)
+    saved_file = default_storage.save(path, file)
+
+    if file.content_type == "image/jpeg":
+        try:
+            fix_jpeg_orientation(path)
+        except (AttributeError, KeyError, IndexError):
+            # cases: image don't have getexif
+            pass  # noqa: WPS420
+
+    return saved_file
 
 
 def delete(path):
