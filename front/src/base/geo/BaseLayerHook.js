c4e70f10ed9da21c926b9e0bd51dfc6b7d774374
Elías Gago <elias.gago@gmail.com>
2023-10-03 11:25:14 +0200

    New base layer OSM-HOT

--- front/src/base/geo/BaseLayerHook.js
+++ front/src/base/geo/BaseLayerHook.js
@@ -4,6 +4,16 @@ import {useCRS} from "./CRSHook";
 
 export const availableBaseLayers = [
     {
+        code: "osm-hot",
+        name: "OpenStreetMap - HOT",
+        isWms: false,
+        url: "https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png",
+        options: {
+            attribution:
+                '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>',
+        },
+    },
+    {
         code: "osm",
         name: "OpenStreetMap",
         isWms: false,
