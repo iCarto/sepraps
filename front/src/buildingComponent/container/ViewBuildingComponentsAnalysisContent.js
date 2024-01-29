f05e377878ea01ce5a26b20502be16d0279e62d3
celiamartinfern <celiamartinfern@gmail.com>
2024-01-29 14:55:35 +0100

    Remove component selector from bc analysis subpage

--- front/src/buildingComponent/container/ViewBuildingComponentsAnalysisContent.js
+++ front/src/buildingComponent/container/ViewBuildingComponentsAnalysisContent.js
@@ -1,37 +1,18 @@
-import {useLocation, useOutletContext, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 import {ViewBuildingComponentsFinancialChart} from "buildingComponent/container";
 import {SectionCard} from "base/ui/section/components";
-import {SubpageWithSelectorContainer} from "base/ui/main";
-import {ComponentListSelector} from "component/presentational";
 
 const ViewBuildingComponentsAnalysisContent = () => {
-    const {bcMonitorings} = useOutletContext();
-    const {id: projectId, buildingComponentId} = useParams();
-    const location = useLocation();
-
-    const isRootPath =
-        location.pathname.split("/").slice(-1)[0] === "buildingcomponents";
+    const {id: projectId} = useParams();
 
     return (
-        <SubpageWithSelectorContainer
-            itemsName="componentes de construcción"
-            itemSelector={
-                <ComponentListSelector
-                    components={bcMonitorings}
-                    basePath={`/projects/list/${projectId}/buildingcomponents`}
-                    selectedComponentId={parseInt(buildingComponentId)}
-                />
-            }
-            noItems={isRootPath && bcMonitorings && bcMonitorings.length === 0}
-        >
-            <SectionCard>
-                <ViewBuildingComponentsFinancialChart
-                    filter={{project: projectId}}
-                    displayGroupedBy
-                />
-            </SectionCard>
-        </SubpageWithSelectorContainer>
+        <SectionCard>
+            <ViewBuildingComponentsFinancialChart
+                filter={{project: projectId}}
+                displayGroupedBy
+            />
+        </SectionCard>
     );
 };
 
