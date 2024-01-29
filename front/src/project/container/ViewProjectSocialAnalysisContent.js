7233e9b207c58927a85b50581887838e4db63b56
celiamartinfern <celiamartinfern@gmail.com>
2024-01-29 15:01:28 +0100

    Remove component selector from sc analysis subpage

--- front/src/project/container/ViewProjectSocialAnalysisContent.js
+++ front/src/project/container/ViewProjectSocialAnalysisContent.js
@@ -1,47 +1,27 @@
-import {useLocation, useOutletContext, useParams} from "react-router-dom";
+import {useParams} from "react-router-dom";
 
 import {
     ViewSocialComponentsTrainingsChart,
     ViewSocialComponentsTrainingsTotalsTable,
 } from "socialComponent/container";
 
-import {SubpageWithSelectorContainer} from "base/ui/main";
 import {SectionCard} from "base/ui/section/components";
-import {ComponentListSelector} from "component/presentational";
 import Stack from "@mui/material/Stack";
 
 const ViewProjectSocialAnalysisContent = () => {
-    const {scMonitorings} = useOutletContext();
-    const {id: projectId, socialComponentId} = useParams();
-    const location = useLocation();
-
-    const isRootPath =
-        location.pathname.split("/").slice(-1)[0] === "buildingcomponents";
+    const {id: projectId} = useParams();
 
     return (
-        <SubpageWithSelectorContainer
-            itemsName="componentes sociales"
-            itemSelector={
-                <ComponentListSelector
-                    components={scMonitorings}
-                    basePath={`/projects/list/${projectId}/socialcomponents`}
-                    selectedComponentId={parseInt(socialComponentId)}
-                    reduceItemsFontSize
+        <Stack spacing={2}>
+            <SectionCard>
+                <ViewSocialComponentsTrainingsChart filter={{project: projectId}} />
+            </SectionCard>
+            <SectionCard>
+                <ViewSocialComponentsTrainingsTotalsTable
+                    filter={{project: projectId}}
                 />
-            }
-            noItems={isRootPath && scMonitorings && scMonitorings.length === 0}
-        >
-            <Stack spacing={2}>
-                <SectionCard>
-                    <ViewSocialComponentsTrainingsChart filter={{project: projectId}} />
-                </SectionCard>
-                <SectionCard>
-                    <ViewSocialComponentsTrainingsTotalsTable
-                        filter={{project: projectId}}
-                    />
-                </SectionCard>
-            </Stack>
-        </SubpageWithSelectorContainer>
+            </SectionCard>
+        </Stack>
     );
 };
 
