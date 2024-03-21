10a6070b82ac1491852c63d299facaeb3a9e0ea0
celiamartinfern <celiamartinfern@gmail.com>
2024-03-21 14:22:25 +0100

    Force coodinates fields in one line for bc properties

--- front/src/base/dynamicform/components/DynamicFormFields.js
+++ front/src/base/dynamicform/components/DynamicFormFields.js
@@ -1,3 +1,4 @@
+import {Fragment} from "react";
 import {DynamicFieldUtil, DynamicFormUtil} from "base/dynamicform/utilities";
 import Grid from "@mui/material/Grid";
 
@@ -5,11 +6,31 @@ const DynamicFormFields = ({attributes, columns = 2}) => {
     return (
         <Grid container columnSpacing={1}>
             {Object.entries(DynamicFieldUtil.getOrderedAttributes(attributes)).map(
-                ([attributeName, attributeSchema]) => (
-                    <Grid item xs={12 / columns} key={attributeName}>
-                        {DynamicFormUtil.getFormField(attributeName, attributeSchema)}
-                    </Grid>
-                )
+                ([attributeName, attributeSchema], index) => {
+                    // TO-DO: Find a better solution for this
+                    // Force empty grid item to move Coordenada X to first column
+                    if (DynamicFieldUtil.shouldBeInFirstColumn(attributeName, index)) {
+                        return (
+                            <Fragment key={attributeName}>
+                                <Grid item xs={12 / columns}></Grid>
+                                <Grid item xs={12 / columns}>
+                                    {DynamicFormUtil.getFormField(
+                                        attributeName,
+                                        attributeSchema
+                                    )}
+                                </Grid>
+                            </Fragment>
+                        );
+                    } else
+                        return (
+                            <Grid item xs={12 / columns} key={attributeName}>
+                                {DynamicFormUtil.getFormField(
+                                    attributeName,
+                                    attributeSchema
+                                )}
+                            </Grid>
+                        );
+                }
             )}
         </Grid>
     );
