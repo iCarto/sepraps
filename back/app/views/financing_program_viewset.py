e99b42e136160c4c67df6af990535761eac33aeb
Elías Gago <elias.gago@gmail.com>
2024-03-26 12:28:38 +0100

    Change financing programs viewset to search and order

--- back/app/views/financing_program_viewset.py
+++ back/app/views/financing_program_viewset.py
@@ -1,10 +1,38 @@
+from django_filters import rest_framework as filters
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import permissions, viewsets
+from rest_framework.pagination import PageNumberPagination
 
+from app.base.views.filters import MappedOrderingFilter
 from app.models.financing_program import FinancingProgram
 from app.serializers.financing_program_serializer import FinancingProgramSerializer
+
+
+class FinancingProgramOrderingFilter(MappedOrderingFilter):
+    ordering_field_mappping = {}
+
+
+class FinancingProgramFilter(filters.FilterSet):
+    class Meta(object):
+        model = FinancingProgram
+        fields = ("search",)
+
+    search = filters.CharFilter(method="filter_by_search_text")
+
+    def filter_by_search_text(self, queryset, name, search_text):
+        return queryset.filter(name__icontains=search_text)
 
 
 class FinancingProgramViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = FinancingProgram.objects.all()
     serializer_class = FinancingProgramSerializer
-    permission_classes = [permissions.DjangoModelPermissions]
+    permission_classes = (permissions.DjangoModelPermissions,)
+    pagination_class = PageNumberPagination
+    filterset_class = FinancingProgramFilter
+    filter_backends = (DjangoFilterBackend, FinancingProgramOrderingFilter)
+    ordering_fields = ("name", "short_name")
+
+    def paginate_queryset(self, queryset):
+        if "page" not in self.request.query_params:
+            return None
+        return super().paginate_queryset(queryset)
